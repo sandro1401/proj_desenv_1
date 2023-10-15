431c0ef4a1333b319563be70d6af7cbccbd2080a
Sandro Teixeira <sandro.jteixeira39@gmail.com>
2023-10-14 21:37:25 -0300

    alteração funcao addAluno

--- trainer_squad/negocio/aluno_negocio.js
+++ trainer_squad/negocio/aluno_negocio.js
@@ -6,17 +6,40 @@ const persistencia = require('../persistencia/aluno_persistencia')
 // Iniciando CRUD
 
 // Create
+// async function addAluno(idUsuario, aluno) {
+//     console.log("Conteúdo do objeto aluno recebido:", aluno)
+//     if (aluno && aluno.sexo && aluno.nome && aluno.cpf && aluno.dt_nascimento && aluno.telefone && aluno.email && aluno.status
+//         && aluno.plano && idUsuario) {
+//             try {
+//                 const alunoAdd = await persistencia.addAluno(idUsuario, aluno)
+//                 return alunoAdd
+//             } catch (error) {
+//                 throw error 
+//             }
+//         } else {
+//             const erro = new Error()
+//             erro.message = "Todos os campos são obrigatórios."
+//             erro.status = 400
+//             throw erro
+//         }
+// }
+
 async function addAluno(idUsuario, aluno) {
     console.log("Conteúdo do objeto aluno recebido:", aluno)
     if (aluno && aluno.sexo && aluno.nome && aluno.cpf && aluno.dt_nascimento && aluno.telefone && aluno.email && aluno.status
         && aluno.plano && idUsuario) {
+            const alunoBuscadoPorCpf = await persistencia.buscarAlunoPorCpf(aluno.cpf);
+            if(!alunoBuscadoPorCpf){
             try {
                 const alunoAdd = await persistencia.addAluno(idUsuario, aluno)
-                return alunoAdd
-            } catch (error) {
-                throw error 
+                return alunoAdd                    
+            }catch (error) {
+                throw  error
             }
-        } else {
+        }else{
+            throw { id: 402, mensagem: "Aluno já cadastrado!"}
+        }
+    }else {
             const erro = new Error()
             erro.message = "Todos os campos são obrigatórios."
             erro.status = 400
@@ -86,6 +109,22 @@ async function buscarAlunoPorId(id) {
     } catch { throw error }
 }
 
+async function buscarAlunoPorCpf(cpf) {
+    try {
+        const cpfAluno = await persistencia.buscarAlunoPorCpf(cpf)
+
+        if (!cpfAluno) {
+            const erro = new Error()
+            erro.message = "Id não encontrado."
+            erro.status = 404
+            throw erro
+        }
+
+        return cpfAluno
+    } catch { throw error }
+}
+
+
 // Update
 async function atualizarAluno(id, alunos) {
     if (alunos && alunos.nome && alunos.email) {
@@ -110,6 +149,7 @@ async function atualizarAluno(id, alunos) {
 
 // Delete
 async function deletarAluno(id) {
+    
     try {
         const clienteDeletado =  await persistencia.deletarAluno(id)
 
@@ -124,7 +164,27 @@ async function deletarAluno(id) {
     } catch (error) { throw error }
 }
 
+// async function deletarAluno(id) {
+    
+//     const alunoBuscadpPorId =await buscarAlunoPorId(id)
+//     if (alunoBuscadpPorId){
+//         try {
+//         const clienteDeletado =  await persistencia.deletarAluno(id)
 
+//         if (!clienteDeletado) {
+//             const erro = new Error()
+//             erro.message = "Aluno não encontrado"
+//             erro.status = 404
+//             throw erro}
+//             return clienteDeletado
+
+//         } catch (error) { throw error }
+//         } else{
+//             throw { id: 404, mensagem: `Aluno com ID ${id} não encontrado!` }
+//            }
+  
+    
+// }
 
 module.exports = {
     addAluno,
@@ -132,6 +192,7 @@ module.exports = {
     buscarAlunoPorNome,
     buscarAlunoPorEmail,
     buscarAlunoPorId,
+    buscarAlunoPorCpf,
     atualizarAluno,
     deletarAluno
     
